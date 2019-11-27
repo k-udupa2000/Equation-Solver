9566a38ceb0f887f7611cdf9d862331eba2d3c69
mnkashif <43775821+mnkashif@users.noreply.github.com>
2019-11-27 13:30:36 +0530

    Update Task7.cpp

--- Task7.cpp
+++ Task7.cpp
@@ -1,9 +1,17 @@
-#include"Task7.h"
-// Task_2::Task_2(float A,float B,float C,float D,float E,float F):Task1(log(A)*B-log(D)*E,log(A)*C,log(D)*F)
-// {
-// }
-double Task_7::solveexpo()
-{
-	return this->solve();
+#ifndef _Task_7_h_
+#define _Task_7_h_
+#include<bits/stdc++.h>
+using namespace std;
 
-}
+class Task_7{
+float a,b,c,d,e,f;	
+	
+public:
+
+	Task_7(float A,float B,float C,float D,float E,float F);
+	float solveexpo();
+
+
+
+};
+#endif
