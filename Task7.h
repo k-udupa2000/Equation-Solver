49243e3106ecd720cc3d13d66aa86321e3af466f
mnkashif <43775821+mnkashif@users.noreply.github.com>
2019-11-27 17:07:51 +0530

    Update Task7.h

--- Task7.h
+++ Task7.h
@@ -1,17 +1,11 @@
-#ifndef _Task_7_h_
-#define _Task_7_h_
-#include<bits/stdc++.h>
-using namespace std;
+#include"Base_Task7.h"
+class Task7: public Base_Task7{
+    float a,b,c,d,e,f,sol;
+    public:
 
-class Task_7{
-float a,b,c,d,e,f;	
-	
-public:
-
-	Task_7(float A,float B,float C,float D,float E,float F);
-	float solveexpo();
-
+    Task7(string input);
+    void solveexpo();
+    void printsoln();
 
 
 };
-#endif
