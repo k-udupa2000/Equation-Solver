6570cf8197565cd0527ab9dc054bf69857dcab73
mnkashif <43775821+mnkashif@users.noreply.github.com>
2019-11-27 23:19:24 +0530

    Update Base_Task7.cpp

--- Base_Task7.cpp
+++ Base_Task7.cpp
@@ -3,10 +3,16 @@ Base_Task7::Base_Task7(string input)
 {
 	eq=input;
 }
-vector<string> Base_Task7::formbx(string input)
+vector<string> Base_Task7::formbx(string input1)
 {
 	string A,B,C;
     vector<string> coeff;
+    string input;
+    A=input1.substr(0,input1.find('^'));
+    if(input1[0]=='-')
+        input=input1.substr(1,input1.size()-1);
+    else
+        input=input1;
     int a1=input.find('^');
     int a2=input.find('x');
     A=input.substr(0,a1);
@@ -39,16 +45,22 @@ vector<string> Base_Task7::formbx(string input)
     return coeff;
 
 }
-vector<string> Base_Task7::formbxplusc(string input)
+vector<string> Base_Task7::formbxplusc(string input1)
 {
 	string A,B,C;
     vector<string> coeff;
+    string input;
+    A=input1.substr(0,input1.find('^'));
     
+    if(input1[0]=='-')
+        input=input1.substr(1,input1.size()-1);
+    else
+        input=input1;
     int a1=input.find('^');
     int a2=input.find('x');
     int a3=input.find(')');
     
-    A=input.substr(0,a1);
+    
    
     if(a3==a2+1)//case C=0;
         coeff=formbx(input);
@@ -69,19 +81,29 @@ vector<string> Base_Task7::formbxplusc(string input)
     }
     return coeff;
 }
-vector<string> Base_Task7::formcplusbx(string input)
+vector<string> Base_Task7::formcplusbx(string input1)
 {
 	string A,B,C;
     vector<string> coeff;
-
+    string input;
+    A=input1.substr(0,input1.find('^'));
+    cout<<A;
+    if(input1[0]=='-')
+        input=input1.substr(1,input1.size()-1);
+    else
+        input=input1;
+    
     int a1=input.find('^');
     int a3=input.find('x');
-    A=input.substr(0,a1);
+    
+   
+    
     if(input.find('+')>input.size())//cases- bx,-bx,c-bx
     {
-        if((input.find('-')>input.size()) || (input.find('-')==a1+2 && count(input.begin(),input.begin()+input.size(),'-')==1))//-bx,bx
+       // cout<<'h';
+        if((input.find('-')>input.size()) || (input.find('-')==a1+2 && count(input.begin(),input.begin()+input.size(),'-')==1) )//-bx,bx
         {
-               A=formbx(input)[0];
+               
                B=formbx(input)[1];
                C=formbx(input)[2];
         }
@@ -139,11 +161,12 @@ vector<string> Base_Task7::formc(string input)
 }
 vector<string> Base_Task7::getcoeff(string input)
 {
-	// string input;
-	// input=input1.substr(1,input1.size()-2);
+	//  string input;
+	//  input=input1.substr(1,input1.size()-2);
     int a=input.find('=');
     string a1=input.substr(0,a);
     string a2=input.substr(a+1,input.size()-a-1);
+    
     vector<string> eq;
     vector<string> coeff;
     eq.push_back(a1);
