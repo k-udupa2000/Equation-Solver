9eefec12dffe3a1ec06fd86911bf22126be4f82a
mnkashif <43775821+mnkashif@users.noreply.github.com>
2019-11-27 17:07:21 +0530

    Update Task7.cpp

--- Task7.cpp
+++ Task7.cpp
@@ -1,24 +1,37 @@
 #include"Task7.h"
-Task_7::Task_7(float A,float B,float C,float D,float E,float F)
+Task7::Task7(string input):Base_Task7(input){}
+void Task7::solveexpo()
 {
-	a=A;
-	b=B;
-	c=C;
-	d=D;
-	e=E;
-	f=F;
-}
-float Task_7::solveexpo()
-{
-	float t=b*log(a)-e*log(d);
+    vector<string> tp=this->getcoeff(this->geteq());
+    a=stof(tp[0]);
+    b=stof(tp[1]);
+    c=stof(tp[2]);
+    d=stof(tp[3]);
+    e=stof(tp[4]);
+    f=stof(tp[5]);
+    float t=b*log(a)-e*log(d);
 	float g=f*log(d)-c*log(a);
-	float l;
-	if(t!=0)
-		l=g/t;
-	else
-		l=numeric_limits<float>::max();
-		
 	
-		
-	return l;
+	if(t!=0)
+		sol=g/t;
+	else
+		sol=numeric_limits<float>::max();
+    
+			
+	
+}
+void Task7::printsoln()
+{
+    if(a>0 && d>0)
+    {
+            if(sol!=numeric_limits<float>::max())
+                cout<<"x="<<fixed<<setprecision(4)<<sol<<'\n';
+            else
+                cout<<"No definite solution\n";
+    }
+    else 
+    {
+        cout<<"Invalid input\n";
+    }
+
 }
