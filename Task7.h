59c98c0dc2734f5528b71d220f367c5e4a946fe7
mnkashif <43775821+mnkashif@users.noreply.github.com>
2019-11-27 13:31:18 +0530

    Update Task7.h

--- Task7.h
+++ Task7.h
@@ -1,14 +1,17 @@
-#pragma once
+#ifndef _Task_7_h_
+#define _Task_7_h_
 #include<bits/stdc++.h>
 using namespace std;
-#include"Task1.h"
-class Task_7:public Task1{
-	
+
+class Task_7{
+float a,b,c,d,e,f;	
 	
 public:
-	Task_7(float A,float B,float C,float D,float E,float F):Task1(log(A)*B-log(D)*E,log(A)*C,log(D)*F){}
-	double solveexpo();
+
+	Task_7(float A,float B,float C,float D,float E,float F);
+	float solveexpo();
 
 
 
 };
+#endif
