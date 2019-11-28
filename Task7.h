b42d5af84bb9b67819588674fb5218c1140f2696
mnkashif <43775821+mnkashif@users.noreply.github.com>
2019-11-28 23:13:37 +0530

    Update Task7.h

--- Task7.h
+++ Task7.h
@@ -1,21 +1,17 @@
-#pragma once
-
-#include <iomanip>
-
-#include"Solver.h"
 #include"Base_Task7.h"
-class Task7: public Base_Task7, public Solver
-{
+class Task7: public Base_Task7{
     float a,b,c,d,e,f,sol;
+    string x;
     public:
 
     Task7(string input);
     void solveexpo();
-    void print();
+    
     friend ostream& operator <<(ostream& output,Task7 obj);
     float geta();
     float getd();
     float getsol();
+    string getx();
     
     
     
