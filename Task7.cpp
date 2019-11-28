559a20444760e1ccc272974d8b1b9b986275d33c
mnkashif <43775821+mnkashif@users.noreply.github.com>
2019-11-28 23:14:05 +0530

    Update Task7.cpp

--- Task7.cpp
+++ Task7.cpp
@@ -1,21 +1,29 @@
-#include "Task7.h"
-Task7::Task7(string input) : Base_Task7(input) {}
+#include"Task7.h"
+Task7::Task7(string input):Base_Task7(input){}
 void Task7::solveexpo()
 {
-    vector<string> tp = this->getcoeff(this->geteq());
-    a = stof(tp[0]);
-    b = stof(tp[1]);
-    c = stof(tp[2]);
-    d = stof(tp[3]);
-    e = stof(tp[4]);
-    f = stof(tp[5]);
-    float t = b * log(a) - e * log(d);
-    float g = f * log(d) - c * log(a);
-
-    if (t != 0)
-        sol = g / t;
-    else
-        sol = numeric_limits<float>::max();
+    vector<string> tp=this->getcoeff(this->geteq());
+    a=stof(tp[0]);
+    b=stof(tp[1]);
+    c=stof(tp[2]);
+    d=stof(tp[3]);
+    e=stof(tp[4]);
+    f=stof(tp[5]);
+    float t=b*log(a)-e*log(d);
+	float g=f*log(d)-c*log(a);
+	
+	if(t!=0)
+		sol=g/t;
+	else if(t==0 && g==0)
+		sol=numeric_limits<float>::max();
+    else if(t==0 && g!=0)
+    {
+        x="no";
+    }
+    
+    
+			
+	
 }
 
 float Task7::geta()
@@ -30,24 +38,28 @@ float Task7::getsol()
 {
     return sol;
 }
-ostream &operator<<(ostream &output, Task7 obj)
+string Task7::getx()
 {
+    return x;
+}
+ostream& operator << (ostream& output,Task7  obj){
     obj.solveexpo();
-    if (obj.geta() > 0 && obj.getd() > 0)
+    if(obj.geta()>0 && obj.getd()>0)   
     {
-        if (obj.getsol() != numeric_limits<float>::max())
-            output << setprecision(4) << "x=" << fixed << setprecision(4) << obj.getsol();
+        
+        if(obj.getsol()==numeric_limits<float>::max())
+            output<<"Infinite solutions\n";
+        else if(obj.getx().compare("no")==0)
+            output<<"No Solution\n";
         else
-            output << "No definite solution" << endl;
+            output<<"x="<<fixed<<setprecision(4)<<obj.getsol()<<'\n';
+        
     }
     else
     {
-        output << "Invalid input" << endl;
+        output<<"Invalid input\n";
     }
     return output;
 }
+    
 
-void Task7 ::print()
-{
-    cout << *this;
-}
